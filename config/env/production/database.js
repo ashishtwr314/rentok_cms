f7297d6db56176e5bdbd13e17db3956582aca8eb
Ashish Tiwari <ashishtwr314@gmail.com>
2022-12-10 09:59:17 +0530

    Digital Ocean, file changes

---
+++ config/env/production/database.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env(
+        "DATABASE_HOST",
+        "db-postgresql-blr1-87334-do-user-8535847-0.b.db.ondigitalocean.com"
+      ),
+      port: env.int("DATABASE_PORT", 25060),
+      database: env("DATABASE_NAME", "defaultdb"),
+      user: env("DATABASE_USERNAME", "doadmin"),
+      password: env("DATABASE_PASSWORD", "AVNS_f9ny2EdaAf3gSsJH99a"),
+      // schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        ca: env("DATABASE_CA"),
+        // rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
+    },
+    debug: false,
+    acquireConnectionTimeout: 300000,
+  },
+});

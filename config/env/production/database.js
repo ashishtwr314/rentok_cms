409535d7302a0004e5af0b884ed6377c0d8f0255
Ashish Tiwari <ashishtwr314@gmail.com>
2022-12-10 14:25:31 +0530

    removed all envs

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,44 @@
+// const fs = require("fs");
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env(
+//         "DATABASE_HOST",
+//         "db-postgresql-blr1-87334-do-user-8535847-0.b.db.ondigitalocean.com"
+//       ),
+//       port: env.int("DATABASE_PORT", 25060),
+//       database: env("DATABASE_NAME", "defaultdb"),
+//       user: env("DATABASE_USERNAME", "doadmin"),
+//       password: env("DATABASE_PASSWORD", "AVNS_f9ny2EdaAf3gSsJH99a"),
+//       ssl: {
+//         ca: fs.readFileSync(`${__dirname}/ca_cert.crt`).toString(),
+//       },
+//       // schema: env("DATABASE_SCHEMA", "public"), // Not required
+//       // ssl: {
+//       //   ca: env("DATABASE_CA"),
+//       //   // rejectUnauthorized: false,
+//       // },
+//     },
+//     debug: false,
+//     acquireConnectionTimeout: 300000,
+//   },
+// });
+
 const fs = require("fs");
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "db-postgresql-blr1-87334-do-user-8535847-0.b.db.ondigitalocean.com"
-      ),
-      port: env.int("DATABASE_PORT", 25060),
-      database: env("DATABASE_NAME", "defaultdb"),
-      user: env("DATABASE_USERNAME", "doadmin"),
-      password: env("DATABASE_PASSWORD", "AVNS_f9ny2EdaAf3gSsJH99a"),
+      host: "db-postgresql-blr1-87334-do-user-8535847-0.b.db.ondigitalocean.com",
+
+      port: 25060,
+      database: "defaultdb",
+      user: "doadmin",
+      password: "AVNS_f9ny2EdaAf3gSsJH99a",
       ssl: {
         ca: fs.readFileSync(`${__dirname}/ca_cert.crt`).toString(),
       },
-      // schema: env("DATABASE_SCHEMA", "public"), // Not required
-      // ssl: {
-      //   ca: env("DATABASE_CA"),
-      //   // rejectUnauthorized: false,
-      // },
     },
     debug: false,
     acquireConnectionTimeout: 300000,

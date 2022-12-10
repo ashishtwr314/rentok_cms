e4489928ee2a81351c8c032e282acce32ceef7ba
Ashish Tiwari <ashishtwr314@gmail.com>
2022-12-10 17:37:23 +0530

    added Amazon RDS db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -70,7 +70,7 @@ module.exports = ({ env }) => ({
         "database-1.cqfiotqwxeci.ap-south-1.rds.amazonaws.com"
       ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "database-1"),
+      database: env("DATABASE_NAME", "postgres"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "AVNS_f9ny2EdaAf3gSsJH99a"),
     },

0fd429e39f9ad23ae7e31ae5860d97884a0af817
Ashish Tiwari <ashishtwr314@gmail.com>
2022-12-19 18:45:25 +0530

    OTP senidng feature enabled, bundleRelease 1

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,46 @@
 "use strict";
 
+const order = require("../../order/controllers/order");
+const dayjs = require("dayjs");
+
 /**
  * product controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::product.product");
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    // const orderEntity = await strapi
+    //   .service("api::order.order", {
+    //     populate: "*",
+    //   })
+    //   .find();
+
+    const orderEntity = await strapi.entityService.findMany(
+      "api::order.order",
+      {
+        populate: ["product"],
+      }
+    );
+
+    const orders = await this.sanitizeOutput(orderEntity, ctx);
+    let products = data;
+    const today = new Date();
+
+    orders.forEach((order) => {
+      products.forEach((product) => {
+        if (product.id === order.product.id) {
+          let minutesLeft = dayjs(order.endDate).diff(dayjs(), "minutes");
+          if (minutesLeft) {
+            product.attributes["activeOrder"] = minutesLeft;
+          }
+        }
+      });
+    });
+
+    return { data: products, meta };
+  },
+}));
--- src/api/sendotp/controllers/sendotp.js
+++ src/api/sendotp/controllers/sendotp.js
@@ -1,11 +1,17 @@
 "use strict";
 
 const otpgenerator = require("otp-generator");
+const fetch = require("cross-fetch");
 /**
  * A set of functions called "actions" for `sendotp`
  */
 
+// const fetch = require("node-fetch");
+
 const OTP_MAP = {};
+
+const FAST_2_SMS_API_KEY =
+  "6EJFzpmhXGMBlROAVjexuIft4S9cN2KZTU0b3g1Ldq8onCQ5sD6SfIYjQdi4szPN0bnwFkBaK7XpctMu";
 module.exports = {
   send: async (ctx, next) => {
     try {
@@ -23,11 +29,26 @@ module.exports = {
 
       OTP_MAP[mobileNum] = otp;
 
-      ctx.body = {
-        data: OTP_MAP[mobileNum],
-        error: false,
-        message: "OTP sent Successfully",
-      };
+      let sendSMSRes = await fetch(
+        `https://www.fast2sms.com/dev/bulkV2?authorization=${FAST_2_SMS_API_KEY}&message=Welcome to Rent.OK Here is your OTP: ${otp}&language=english&route=q&numbers=${mobileNum}`
+      );
+
+      const sendSMS = await sendSMSRes.json();
+      console.log(sendSMS);
+
+      if (sendSMS?.return) {
+        ctx.body = {
+          data: OTP_MAP[mobileNum],
+          error: false,
+          message: "OTP sent Successfully",
+        };
+      } else {
+        ctx.body = {
+          data: null,
+          error: true,
+          message: `Error sending OTP, Try again later, btw your OTP IS ${otp}`,
+        };
+      }
     } catch (err) {
       console.log(err);
       ctx.body = err;
